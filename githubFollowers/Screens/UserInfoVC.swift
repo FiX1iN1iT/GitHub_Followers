7d33b1cef61326776f2ed49923eb04e57592f36b
Alexander <fix1in1it@gmail.com>
2023-08-19 22:55:05 +0300

    user info screen

--- githubFollowers/Screens/UserInfoVC.swift
+++ githubFollowers/Screens/UserInfoVC.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class UserInfoVC: UIViewController {
+    
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+    }
+    
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
     }
 }
